cfd6eac07ee15cf12d8e0a2a204845625646b513
Nainnvn <botfb84@gmail.com>
2024-12-19 14:14:39 +0530

    Update 0owner.js

--- scripts/cmds/0owner.js
+++ scripts/cmds/0owner.js
@@ -20,9 +20,11 @@ config: {
       gender: 'Male',
       age: '24',
       height: '5.10',
+      facebookLink: 'https://www.facebook.com/julmijaat1008?mibextid=ZbWKwL',
+      nickname: 'JULMI'
     };
 
-    const bold = 'https://i.imgur.com/DDO686J.mp4'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
+    const bold = 'https://i.imgur.com/HnxHi6i.jpeg'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
 
     const tmpFolderPath = path.join(__dirname, 'tmp');
 
